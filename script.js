02b236deebc1c3c9bcd8b72229e411eea40fafae
MTLY-1610 <larrouy.m@me.com>
2020-01-23 18:51:32 +0100

    last laptop commit

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const swup = new swup()
+
+function hemma() {
+  document.getElementById("swup").innerHTML = "<div  id='h'class='hero'><img class='hero_logo' src='JUTA_logo_hero.svg'></div>";
+}
+
+function omoss() {
+  document.getElementById("swup").innerHTML = "<div id='o' class='wrapper'><p>Med fokus på den mänskliga upplevelsen av rum, invändiga som utvändiga, skapar vi nytt och utvecklar befintliga värden, tillsammans. En arkitektur med ett tydligt syfte för en hållbar framtid.</br>Har du en idé eller behöver komma vidare i en byggprocess? Hör av er och vi ser hur vi kan bidra till ert projekt.</p><i id='hej' class='fal fa-envelope'></i></div>";
+}
+function kontakt() {
+  document.getElementById("swup").innerHTML = "<div id='k' class='main_kontakt'><div class='wrapper_kontakt'><div class='name_kontakt'>EMMA LARROUY</div><div class='jobb'>Arkitekt</div><div class='text_kontakt'>emma@juta-arkitekter.se </div></div><div class='wraper_kontakt'><div class='name_kontakt'>PETER SELSVIK</div><div class='jobb'>Byggnadsingenjör</div><div class='text_kontakt'>peter@juta-arkitekter.se </div></div><div class='wraper_kontakt'><div class='name_kontakt'>MAGDALENA KELLER</div><div class='jobb'>Koncept & Inredningsdesigner</div><div class='text_kontakt'>magdalena@juta-arkitekter.se </div><div class='text_kontakt'>Tel 070-471 40 24</div></div></div>";
+}
+
+
