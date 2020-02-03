037e7678cf12e457b78f6f8d5079d16560040d78
MTLY-1610 <larrouy.m@me.com>
2020-02-03 13:50:38 +0100

    safari

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 
 function hemma() {
-  document.getElementById("swup").innerHTML = "<div  id='h'class='hero'><img class='hero_logo' src='JUTA_logo_hero.svg'></div>";
+  document.getElementById("swup").innerHTML = "<img class='hero_logo' src='JUTA_logo_hero.svg'>";
 }
 
 function omoss() {
-  document.getElementById("swup").innerHTML = "<div id='o' class='wrapper'><p>Med fokus på den mänskliga upplevelsen av rum, invändiga som utvändiga, skapar vi nytt och utvecklar befintliga värden, tillsammans. En arkitektur med ett tydligt syfte för en hållbar framtid.</br>Har du en idé eller behöver komma vidare i en byggprocess? Hör av er och vi ser hur vi kan bidra till ert projekt.</p><a href='mailto:emma@juta-arkitekter.se;subject=Hej%20lanken%20from%20site'><i id='hej'class='fad fa-paper-plane'></i></a></div>";
+  document.getElementById("swup").innerHTML = "<div id='o' class='wrapper'><p>Med fokus på den mänskliga upplevelsen av rum, skapar vi nytt och utvecklar befintliga värden, tillsammans. En arkitektur med ett tydligt syfte för en hållbar framtid.</br>Har du en idé eller behöver komma vidare i en byggprocess? Hör av er och vi ser hur vi kan bidra till era projekt.</p><a href='mailto:emma@juta-arkitekter.se;subject=Hej%20lanken%20from%20site'><i id='hej'class='fad fa-paper-plane'></i></a></div>";
 }
 function kontakt() {
   document.getElementById("swup").innerHTML = "<div id='k' class='main_kontakt'><div class='wrapper_kontakt'><div class='name_kontakt'><h3>EMMA LARROUY</h3></div><div class='jobb'>Arkitekt SAR/MSA</div><div class='mail_kontakt'><a href='mailto:emma@juta-arkitekter.se?cc=magdalena.keller@gmail.com&amp;subject=Till%20Keller%20from%20site'>emma@juta-arkitekter.se </a></div><div class='tel_kontakt'>Tel 070-879 96 03</div></div><div class='wraper_kontakt'><div class='name_kontakt'><h3>PETER SELSVIK</h3></div><div class='jobb'>Byggnadsingenjör</div><div class='mail_kontakt'><a href='mailto:emma@juta-arkitekter.se?cc=peter.selsvik@telia.com&amp;subject=till%20Peter%20from%20site'>peter@juta-arkitekter.se</a></div></div><div class='wraper_kontakt'><div class='name_kontakt'><h3>MAGDALENA KELLER</h3></div><div class='jobb'>Koncept & Inredningsdesigner</div><div class='mail_kontakt'><a href='mailto:emma@juta-arkitekter.se?cc=magdalena.keller@gmail.com&amp;subject=Till%20Keller%20from%20site'>magdalena@juta-arkitekter.se</a></div><div class='tel_kontakt'>Tel 070-471 40 24</div></div></div>";
+}
+function menuMobile() {
+  document.getElementById("swup").innerHTML = "<div class='mobileMenu'><div class='mobileMenuTitle'>HEMMA</div><div class='mobileMenuTitle'><a id='topMenu' onclick='omoss()'>OM OSS</a></div><div class='mobileMenuTitle'>KONTAKT</div> </div>";
 }
 
 
