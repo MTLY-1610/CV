962ad6acb243ab03db438454f10297fb63ef74bb
MTLY-1610 <57067429+MTLY-1610@users.noreply.github.com>
2020-05-24 13:40:33 +0200

    pie chart script

---
+++ chart.js
@@ -0,0 +1,65 @@
+let total = 158,
+    buttons = document.querySelector('.button'),
+    pie = document.querySelector('.pies'),
+    activeClass = 'active';
+
+let skills = {
+    SWEDISH: 80,
+    ENGLISH : 90,
+    FRENCH: 100,
+    GERMAN: 70,
+    ILLUSTRATOR: 80,
+    PHOTOSHOP: 70,
+    INDESIGN: 70,
+    XD:60,
+    HTML:70,
+    CSS:70,
+    JAVASCRIPT:40,
+    TYPESCRIPT: 40,
+  };
+  
+  
+  /** work out percentage as a result of total */
+  let numberFixer = function(num){
+    let result = ((num * total) / 100);
+    return result;
+  }
+  
+  
+  /** create a button for each slills*/
+  for(property in skills){
+    let newEl = document.createElement('button');
+    newEl.innerText = property;
+    newEl.setAttribute('data-name', property);
+    buttons.appendChild(newEl);
+  }
+  
+  /** when you click a button setPieChart and setActiveClass */
+    buttons.addEventListener('click', function(e){
+      if(e.target != e.currentTarget){
+        let el = e.target,
+            name = el.getAttribute('data-name');
+        setPieChart(name);
+        setActiveClass(el);
+      }
+      e.stopPropagation();
+    });
+  
+  let setPieChart = function(name){
+    let number = skills[name],
+        fixedNumber = numberFixer(number),
+        result = fixedNumber + ' ' + total;
+    
+    pie.style.strokeDasharray = result;
+  }
+  
+  let setActiveClass = function(el) {
+    for(let i = 0; i < buttons.children.length; i++) {
+      buttons.children[i].classList.remove(activeClass);
+      el.classList.add(activeClass);
+    }
+  }
+  
+  /** Set up default settings */
+  setPieChart('SWEDISH');
+  setActiveClass(buttons.children[0]);
--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 
 function hemma() {
-  document.getElementById("swup").innerHTML = "<div class='hero'><img draggable='false' class='hero_logo' src='JUTA_logo_hero3.svg'><img draggable='false' class='mobile_logo2' src='JUTA_logo_hero.svg'><img draggable='false' class='mobile_logo3' src='JUTA_logo_hero4.svg'></div>";
+  document.getElementById("swup").innerHTML = "<div class='hero'><div draggable='false' class='hero_text'>MATHIEU</br>LARROUY</div>";
 }
 
 function omoss() {
-  document.getElementById("swup").innerHTML = "<div id='o' class='wrapper'><p>I am a french Art director and aspiring Front- end developer. Comming from Paris i now reside in Göteborg. After spending 10 years doing anything between graphic design and creative direction, from poster print to digital marketing, I now decided to turn myself toward UX design and Front-end development.</p><a href='mailto:emma@juta-arkitekter.se;subject=Hej%20lanken%20from%20site'><i id='hej'class='fad fa-paper-plane'></i></a></div>";
+  document.getElementById("swup").innerHTML = "<div id='o' class='wrapper'><p>I am a french Art director and aspiring Front- end developer. Comming from Paris I now reside in Göteborg. After spending 10 years doing anything between graphic design and creative direction, I now decided to turn myself toward Front-end development an need a LIA ;)</p></div>";
+}
+function skill() {
+  document.getElementById("swup").innerHTML = "<div id='o' class='wrapper'><div class='charts'><figure><figcaption>LANGUAGES & SOFTWARES</figcaption><div class='button'></div><svg width='100' height='100' class='chart'><circle r='25' cx='50' cy='50' class='pies' /></svg></figure></div>";
 }
 function kontakt() {
-  document.getElementById("swup").innerHTML = "<div class='main_kontakt'><div class='wrapper_kontakt'><div class='name_kontakt'><h3>EMMA LARROUY</h3></div><div class='jobb'>Arkitekt SAR/MSA</div><div class='mail_kontakt'><a href='mailto:emma@juta-arkitekter.se?cc=magdalena.keller@gmail.com&amp;subject=Till%20Keller%20from%20site'>emma@juta-arkitekter.se </a></div><div class='tel_kontakt'><a href='tel:+46 708799603'>Tel 070-879 96 03</a></div></div><div class='wraper_kontakt'><div class='name_kontakt'><h3>PETER SELSVIK</h3></div><div class='jobb'>Byggnadsingenjör</div><div class='mail_kontakt'><a href='mailto:emma@juta-arkitekter.se?cc=peter.selsvik@telia.com&amp;subject=till%20Peter%20from%20site'>peter@juta-arkitekter.se</a></div></div><div class='wraper_kontakt'><div class='name_kontakt'><h3>MAGDALENA KELLER</h3></div><div class='jobb'>Koncept & Inredningsdesigner</div><div class='mail_kontakt'><a href='mailto:emma@juta-arkitekter.se?cc=magdalena.keller@gmail.com&amp;subject=Till%20Magdelena%20from%20site'>magdalena@juta-arkitekter.se</a></div><div class='tel_kontakt'><a href='tel:+46 704714024'>Tel 070-471 40 24</a></div></div></div>";
+  document.getElementById("swup").innerHTML = "<div class='main_kontakt'><div class='wrapper_kontakt'><div class='name_kontakt'><h3>MATHIEU LARROUY</h3></div><div class='mail_kontakt'><a href='mailto:larrouy.m@gmail.com?cc=larrouy.m@me.com&amp;subject=Till%20moi%20from%20site'>larrouy.m@gmail.com </a></div><div class='tel_kontakt'><a href='tel:+46 708799603'>Tel 076-816 75 00</a></div></div></div>";
 }
 function menuMobile() {
   document.getElementById("swup").innerHTML = "<div class='mobileMenu'><div class='mobileMenuTitle'><a onclick='hemma()'>HEMMA</a></div><div class='mobileMenuTitle'><a onclick='omoss()'>OM OSS</a></div><div class='mobileMenuTitle'><a onclick='kontakt()'>KONTAKT</a></div> </div>";
