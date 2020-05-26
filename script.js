b3bfac06192aeec28d72572689b54291d03ba0ca
MTLY-1610 <57067429+MTLY-1610@users.noreply.github.com>
2020-05-26 11:23:53 +0200

    menu change

--- script.js
+++ script.js
@@ -2,18 +2,17 @@
 function hemma() {
   document.getElementById("swup").innerHTML = "<div class='hero'><div draggable='false' class='hero_text'>MATHIEU</br>LARROUY</div>";
 }
-
 function omoss() {
   document.getElementById("swup").innerHTML = "<div id='o' class='wrapper'><p>I am a french Art director and aspiring Front- end developer. Comming from Paris I now reside in Göteborg. After spending 10 years doing anything between graphic design and creative direction, I now decided to turn myself toward Front-end development an need a LIA ;)</p></div>";
 }
+
 function skill() {
-  document.getElementById("swup").innerHTML = "<div id='o' class='wrapper'><div class='charts'><figure><figcaption>LANGUAGES & SOFTWARES</figcaption><div class='button'></div><svg width='100' height='100' class='chart'><circle r='25' cx='50' cy='50' class='pies' /></svg></figure></div>";
+  document.getElementById("swup").innerHTML = "<div id='o' class='wrapper'><div class='graph-cont'><h3>HTML5</h3><div class='bar bar1'></div></div><div class='graph-cont'><h3>CSS3</h3><div class='bar bar2'></div></div><div class='graph-cont'><h3>JAVASCRIPT</h3><div class='bar bar3'><div class='graph-cont'><h3>TYPESCRIPT</h3><div class='bar bar4'></div></div></div></div><div class='graph-cont'><h3>CSS3</h3><div class='bar bar5'></div></div></div>";
 }
 function kontakt() {
-  document.getElementById("swup").innerHTML = "<div class='main_kontakt'><div class='wrapper_kontakt'><div class='name_kontakt'><h3>MATHIEU LARROUY</h3></div><div class='mail_kontakt'><a href='mailto:larrouy.m@gmail.com?cc=larrouy.m@me.com&amp;subject=Till%20moi%20from%20site'>larrouy.m@gmail.com </a></div><div class='tel_kontakt'><a href='tel:+46 708799603'>Tel 076-816 75 00</a></div></div></div>";
+  document.getElementById("swup").innerHTML = "<div class='chart-wrapper'></div><button onclick='replay()'>Replay</button></div>";
 }
 function menuMobile() {
   document.getElementById("swup").innerHTML = "<div class='mobileMenu'><div class='mobileMenuTitle'><a onclick='hemma()'>HEMMA</a></div><div class='mobileMenuTitle'><a onclick='omoss()'>OM OSS</a></div><div class='mobileMenuTitle'><a onclick='kontakt()'>KONTAKT</a></div> </div>";
 }
-
 
