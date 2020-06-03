6d96c4655e6bb34ed6957c662fc23681f10eeaf3
MTLY-1610 <57067429+MTLY-1610@users.noreply.github.com>
2020-06-03 10:36:16 +0200

    latest

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ function hemma() {
   document.getElementById("swup").innerHTML = "<div class='hero'><div draggable='false' class='hero_text'>MATHIEU</br>LARROUY</div>";
 }
 function omoss() {
-  document.getElementById("swup").innerHTML = "<div id='o' class='wrapper'><p>I am a french Art director,</br>now aspiring Front - end developer. I comme from Paris,  and now reside in Göteborg. After spending 10 years doing anything between graphic design and creative direction, I now decided to turn myself toward Front-end development an need a LIA ;)</p></div>";
+  document.getElementById("swup").innerHTML = "<div  class='wrapper'><p>I am a french Art director,</br>now aspiring Front - end developer. I comme from Paris,  and now reside in Göteborg. After spending 10 years doing anything between graphic design and creative direction, I now decided to turn myself toward Front-end development an need a LIA ;)</p></div>";
 }
 
 function skill() {
-  document.getElementById("swup").innerHTML = "<div id='o' class='wrapper'><div class='graph-cont'><h3>HTML5</h3><div class='bar bar1'></div></div>/div></div>";
+  document.getElementById("swup").innerHTML = "<div id='o' class='wrapper'><div class='flexbox'><div class='graph-cont'><h3>HTML&CSS</h3><div class='bar bar1'></div></div><div class='graph-cont'><h3>SCSS</h3><div class='bar bar2'></div></div><div class='graph-cont'><h3>SCSS</h3><div class='bar bar2'></div></div><div class='graph-cont'><h3>SCSS</h3><div class='bar bar2'></div></div><div class='graph-cont'><h3>SCSS</h3><div class='bar bar2'></div></div></div><div class='flexbox'><div class='graph-cont'><h3>HTML&CSS</h3><div class='bar bar1'></div></div><div class='graph-cont'><h3>SCSS</h3><div class='bar bar2'></div></div><div class='graph-cont'><h3>SCSS</h3><div class='bar bar2'></div></div><div class='graph-cont'><h3>SCSS</h3><div class='bar bar2'></div></div><div class='graph-cont'><h3>SCSS</h3><div class='bar bar2'></div></div></div></div>";
 }
 function kontakt() {
-  document.getElementById("swup").innerHTML = "<div class='chart-wrapper'></div><button onclick='replay()'>Replay</button></div>";
+  document.getElementById("swup").innerHTML = "<div class='wrapper'></div>";
 }
 function menuMobile() {
   document.getElementById("swup").innerHTML = "<div class='mobileMenu'><div class='mobileMenuTitle'><a onclick='hemma()'>HEMMA</a></div><div class='mobileMenuTitle'><a onclick='omoss()'>OM OSS</a></div><div class='mobileMenuTitle'><a onclick='kontakt()'>KONTAKT</a></div> </div>";
