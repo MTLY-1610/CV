201dfacebed18a42888f65cd387b8b9678a0a2b5
MTLY-1610 <57067429+MTLY-1610@users.noreply.github.com>
2020-06-07 15:50:44 +0200

    kontakt link

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ function omoss() {
 }
 
 function skill() {
-  document.getElementById("swup").innerHTML = "<div id='o' class='wrapper'><div class='flexbox'><div class='graph-cont'><h3>HTML&CSS</h3><div class='bar bar1'></div></div><div class='graph-cont'><h3>SCSS</h3><div class='bar bar2'></div></div><div class='graph-cont'><h3>SCSS</h3><div class='bar bar2'></div></div><div class='graph-cont'><h3>SCSS</h3><div class='bar bar2'></div></div><div class='graph-cont'><h3>SCSS</h3><div class='bar bar2'></div></div></div><div class='flexbox'><div class='graph-cont'><h3>HTML&CSS</h3><div class='bar bar1'></div></div><div class='graph-cont'><h3>SCSS</h3><div class='bar bar2'></div></div><div class='graph-cont'><h3>SCSS</h3><div class='bar bar2'></div></div><div class='graph-cont'><h3>SCSS</h3><div class='bar bar2'></div></div><div class='graph-cont'><h3>SCSS</h3><div class='bar bar2'></div></div></div></div>";
+  document.getElementById("swup").innerHTML = "<div id='o' class='wrapper'><div class='flexbox'><div class='graph-cont'><h3>HTML&CSS</h3><div class='bar bar1'></div></div><div class='graph-cont'><h3>SCSS</h3><div class='bar bar2'></div></div><div class='graph-cont'><h3>Javascript</h3><div class='bar bar3'></div></div><div class='graph-cont'><h3>Typescript</h3><div class='bar bar4'></div></div><div class='graph-cont'><h3>React</h3><div class='bar bar5'></div></div></div><div class='flexbox'><div class='graph-cont'><h3>Aurelia</h3><div class='bar bar6'></div></div><div class='graph-cont'><h3>Express & MongoDb</h3><div class='bar bar7'></div></div><div class='graph-cont'><h3>English</h3><div class='bar bar8'></div></div><div class='graph-cont'><h3>Swedish</h3><div class='bar bar9'></div></div><div class='graph-cont'><h3>Adobe XD</h3><div class='bar bar10'></div></div></div></div>";
 }
 function kontakt() {
-  document.getElementById("swup").innerHTML = "<div class='wrapper'></div>";
+  document.getElementById("swup").innerHTML = "<div class='wrapper'> <div class='wrapper_kontakt'> <div class='text_kontakt'> </div><div class='icon_kontakt'> <a href='https://www.linkedin.com/in/mathieularrouy/'><i id='' class='fab fa-linkedin-in'></i></a><a id='' href='tel:+46768167500'><i class='fad fa-mobile-alt'></i></a><a href='https://github.com/MTLY-1610'><i class='fab fa-github-alt'></i></a><a href='mailto:larrouy.m@gmail.com;?subject=LIA'><i class='fal fa-paper-plane'></i></a></div></div></div>";
 }
 
 
